e11872a90a45c236d16ece84318652258da43d5f
SeanZhouSiyuan <zhousiyuan12321@gmail.com>
2019-01-12 10:22:51 +0800

    added intro styles

--- src/components/PageIntro.vue
+++ src/components/PageIntro.vue
@@ -1,16 +1,22 @@
 <template>
     <div id="page_intro" class="page">
-        <div id="bio">
-            <h2 id="bio-heading">About Me</h2>
-            <p id="bio-content">I'm Sean Zhou, a student and web developer. I study Information Technology at the Hong Kong University of Science and Technology, and previously went to Shenzhen University for undergraduate degree. I have been learning front-end as well as back-end, mainly Node.js and its friends. I like swimming, running, and Cantonese food. I believe in love.</p>
-        </div>
-        <div id="issues-box">
-            <div
-                v-for="(item, index) in issues"
-                :key="index"
-                class="single-issue"
-            >
-                <h3>{{ item }}</h3>
+        <div class="wrapper">
+            <div id="bio">
+                <h2 id="bio_heading">About Me</h2>
+                <p id="bio_content">{{ bio }}</p>
+                <p id="bio_tip">To learn more about me, click on the issues.</p>
+            </div>
+            <div id="issues_box">
+                <div
+                    v-for="(item, index) in issues"
+                    :key="index"
+                    class="issue"
+                >
+                    <div class="issue-icon">
+                        <span :class="['fas', `fa-${item.icon}`]"></span>
+                    </div>
+                    <h3 class="issue-text">{{ item.text }}</h3>
+                </div>
             </div>
         </div>
     </div>
@@ -21,11 +27,18 @@ export default {
     name: 'PageIntro',
     data() {
         return {
+            bio: 'I\'m Sean Zhou, a student and web developer. I study computer science at HKUST, and previously went to Shenzhen University for undergraduate degree. I have been learning front-end as well as back-end, mainly Node.js. I like swimming, running, and Cantonese food. I believe in love.',
             issues: [
-                'Education',
-                'Skills',
-                'Experiences',
-                'Honors'
+                {
+                    icon: 'book-open',
+                    text: 'Education'
+                }, {
+                    icon: 'code',
+                    text: 'Skills'
+                }, {
+                    icon: 'cogs',
+                    text: 'Experiences'
+                }
             ]
         }
     }

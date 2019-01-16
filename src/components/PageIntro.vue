70399a7029232a7dc2da877d1fb1cdf6065e7177
SeanZhouSiyuan <zhousiyuan12321@gmail.com>
2019-01-16 18:02:23 +0800

    added close button

--- src/components/PageIntro.vue
+++ src/components/PageIntro.vue
@@ -12,7 +12,7 @@
                     class="category"
                 >
                     <div class="category-icon">
-                        <span :class="['fas', `fa-${item.icon}`]"></span>
+                        <i :class="['fas', `fa-${item.icon}`]"></i>
                     </div>
                     <h3 class="category-text">{{ item.text }}</h3>
                 </div>
@@ -35,6 +35,9 @@
                             </div>
                         </div>
                     </div>
+                    <svg viewBox="0 0 32 32" id="entries_close">
+                        <path d="M 7.21875 5.78125 L 5.78125 7.21875 L 14.5625 16 L 5.78125 24.78125 L 7.21875 26.21875 L 16 17.4375 L 24.78125 26.21875 L 26.21875 24.78125 L 17.4375 16 L 26.21875 7.21875 L 24.78125 5.78125 L 16 14.5625 Z "/>
+                    </svg>
                 </div>
             </div>
         </div>

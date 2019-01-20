bbca7edf88a50583531065ea4791f638a7080abc
SeanZhouSiyuan <zhousiyuan12321@gmail.com>
2019-01-20 11:13:01 +0800

    added dynamic content & transition

--- src/components/PageIntro.vue
+++ src/components/PageIntro.vue
@@ -9,24 +9,28 @@
                 <div
                     v-for="(item, index) in categories"
                     :key="index"
+                    @click="toggleOverlay(index)"
                     class="category"
                 >
                     <div class="category-icon">
                         <i :class="['fas', `fa-${item.icon}`]"></i>
                     </div>
-                    <h3 class="category-text">{{ item.text }}</h3>
+                    <h3 class="category-text">{{ item.title }}</h3>
                 </div>
             </div>
-            <div id="intro_overlay">
+            <div :style="overlay.styles" id="intro_overlay">
                 <div id="overlay_wrapper" class="wrapper">
                     <div id="entries_box">
-                        <div class="timeline">
+                        <div :class="{timeline: overlay.hasTimeline}">
                             <div
-                                v-for="(item, index) in edu"
+                                v-for="(item, index) in overlay.content"
                                 :key="index"
                                 class="entry"
                             >
-                                <svg class="indicator">
+                                <svg
+                                    v-if="overlay.hasTimeline"
+                                    class="indicator"
+                                >
                                     <circle cx="6" cy="6" r="5" />
                                 </svg>
                                 <p>{{ item.period }}</p>
@@ -35,7 +39,10 @@
                             </div>
                         </div>
                     </div>
-                    <svg viewBox="0 0 32 32" id="entries_close">
+                    <svg
+                        viewBox="0 0 32 32"
+                        id="entries_close"
+                        @click="toggleOverlay(-1)">
                         <path d="M 7.21875 5.78125 L 5.78125 7.21875 L 14.5625 16 L 5.78125 24.78125 L 7.21875 26.21875 L 16 17.4375 L 24.78125 26.21875 L 26.21875 24.78125 L 17.4375 16 L 26.21875 7.21875 L 24.78125 5.78125 L 16 14.5625 Z "/>
                     </svg>
                 </div>
@@ -49,56 +56,89 @@ export default {
     name: 'PageIntro',
     data() {
         return {
+            overlay: {
+                content: [],
+                hasTimeline: false,
+                styles: {
+                    display: 'none',
+                    opacity: '0'
+                }
+            },
             categories: [
                 {
                     icon: 'book',
-                    text: 'Education'
+                    title: 'Education',
+                    hasTimeline: true,
+                    content: [
+                        {
+                            entity: 'The Hong Kong University of Science and Technology',
+                            period: '2018.09-present',
+                            desc: 'Master of Science in Information Technology, expected June 2019'
+                        }, {
+                            entity: 'The University of California San Diego',
+                            period: '2017.08-2017.12',
+                            desc: 'Visiting student enrolled in Language and American Culture program'
+                        }, {
+                            entity: 'Shenzhen University',
+                            period: '2014.10-2018.06',
+                            desc: 'Bachelor of Engineering in Telecommunication, with a GPA of 3.8/4.0'
+                        }
+                    ]
                 }, {
                     icon: 'code',
-                    text: 'Skills'
+                    title: 'Skills',
+                    hasTimeline: false,
+                    content: [
+                        'HTML',
+                        'CSS',
+                        'JavaScript',
+                        'Node.js',
+                        'MongoDB'
+                    ]
                 }, {
                     icon: 'cogs',
-                    text: 'Experiences'
+                    title: 'Experiences',
+                    hasTimeline: true,
+                    content: [
+                        {
+                            entity: 'Magnum Research Limited',
+                            period: '2018.10 - present',
+                            title: 'Front-end Intern',
+                            desc: 'Assists in building an performance visualization website using Vue.js and charting libraries'
+                        }, {
+                            entity: 'Haituncun Information Technology Limited',
+                            period: '2017.05 - 2017.08',
+                            title: 'Front-end Intern',
+                            desc: 'Studied asynchronous JavaScript; Assisted in implementing a single-page site with Vue.js'
+                        }
+                    ]
                 }, {
                     icon: 'trophy',
-                    text: 'Honors'
-                }
-            ],
-            edu: [
-                {
-                    entity: 'The Hong Kong University of Science and Technology',
-                    period: '2018.09-present',
-                    desc: 'Master of Science in Information Technology, expected June 2019'
-                }, {
-                    entity: 'The University of California San Diego',
-                    period: '2017.08-2017.12',
-                    desc: 'Visiting student enrolled in Language and American Culture program'
-                }, {
-                    entity: 'Shenzhen University',
-                    period: '2014.10-2018.06',
-                    desc: 'Bachelor of Engineering in Telecommunication, with a GPA of 3.8/4.0'
-                }
-            ],
-            skills: [
-                'HTML',
-                'CSS',
-                'JavaScript',
-                'Node.js',
-                'MongoDB'
-            ],
-            experiences: [
-                {
-                    entity: 'Magnum Research Limited',
-                    period: '2018.10 - present',
-                    title: 'Front-end Intern',
-                    desc: 'Assists in building an performance visualization website using Vue.js and charting libraries'
-                }, {
-                    entity: 'Haituncun Information Technology Limited',
-                    period: '2017.05 - 2017.08',
-                    title: 'Front-end Intern',
-                    desc: 'Studied asynchronous JavaScript; Assisted in implementing a single-page site with Vue.js'
+                    title: 'Honors',
+                    hasTimeline: false,
+                    content: []
                 }
             ]
+        }
+    },
+    methods: {
+        toggleOverlay(index) {
+            let overlay = this.overlay;
+            if (index >= 0) {
+                overlay.content = this.categories[index].content;
+                overlay.hasTimeline = this.categories[index].hasTimeline;
+            }
+            if (overlay.styles.display === 'none') {
+                overlay.styles.display = 'block';
+                setTimeout(() => {
+                    overlay.styles.opacity = '1';
+                });
+            } else {
+                overlay.styles.opacity = '0';
+                setTimeout(() => {
+                    overlay.styles.display = 'none';
+                }, 250);
+            }
         }
     }
 }

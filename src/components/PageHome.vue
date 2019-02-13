2beb621aec9cf4de78c8c12248e2ac9c79b48bbe
SeanZhouSiyuan <zhousiyuan12321@gmail.com>
2019-02-13 18:30:30 +0800

    updated content and styles

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,18 +1,12 @@
 <template>
     <div id="page_home" class="page">
-        <h1 class="title">Sean Zhou</h1>
-        <h2 class="title-secondary">Student, front-end learner, liberal.</h2>
-        <nav>
-            <div class="nav-item">
-                <router-link to="/posts">Blog</router-link>
+        <div id="home_wrapper" class="wrapper">
+            <div class="home-container">
+                <!-- <div class="avatar"></div> -->
+                <h1 class="title">Sean Zhou</h1>
+                <p class="title-secondary">Student, developer, citizen. I like to create.</p>
             </div>
-            <div class="nav-item">
-                <a href="https://github.com/SeanZhouSiyuan">GitHub</a>
-            </div>
-            <div class="nav-item">
-                <a href="#">About</a>
-            </div>
-        </nav>
+        </div>
     </div>
 </template>
 

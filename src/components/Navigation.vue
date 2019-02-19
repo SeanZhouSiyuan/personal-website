c4ad31c126de939d7690602175ef0e02f45c547f
SeanZhouSiyuan <zhousiyuan12321@gmail.com>
2019-02-19 11:26:45 +0800

    removed `xmlns` attributes

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -6,7 +6,7 @@
                 @click="toggleNavigation"
                 :class="['button', menu.open ? 'active' : '']"
             >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <svg viewBox="0 0 24 24">
                     <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
                     <path d="M0 0h24v24H0z" fill="none"/>
                 </svg>
@@ -29,7 +29,7 @@
                             this.menu.submenu.open ? 'active' : ''
                         ]"
                     >
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <svg viewBox="0 0 24 24">
                             <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/>
                             <path fill="none" d="M0 0h24v24H0V0z"/>
                         </svg>

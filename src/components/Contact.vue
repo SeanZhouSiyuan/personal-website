7c49287d85da5b767e243aa525869dfa3efac50a
SeanZhouSiyuan <zhousiyuan12321@gmail.com>
2019-02-22 11:59:39 +0800

    added contact links

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,10 +3,26 @@
         <div class="contact-wrapper wrapper">
             <header class="section-header">
                 <h2 class="section-heading">Let's Get in Touch</h2>
-                <p class="section-intro">Contact me via the fol&shy;low&shy;ing email, or find me on <span>GitHub</span>, <span>Facebook</span>, and <span>WeChat</span>.</p>
+                <p class="section-intro">Contact me via the fol&shy;low&shy;ing email, or find me on <a href="https://github.com/SeanZhouSiyuan" target="_blank" class="contact">GitHub</a>, <a href="https://www.facebook.com/seanzhousiyuan" target="_blank" class="contact">Facebook</a>, and <span @click="toggleOverlay" class="contact contact--wechat">WeChat</span>.</p>
+                <div :class="['overlay', this.overlay.open ? 'open' : '']">
+                    <header class="overlay-header">
+                        <h2 class="overlay-title">Scan to add me on WeChat</h2>
+                    </header>
+                    <div class="img-container">
+                        <img src="./../assets/imgs/qr_code_wechat.jpg" alt="WeChat QR code">
+                    </div>
+                    <div class="overlay-control">
+                        <svg
+                            viewBox="0 0 32 32"
+                            id="overlay_button"
+                            @click="toggleOverlay(-1)">
+                            <path d="M 7.21875 5.78125 L 5.78125 7.21875 L 14.5625 16 L 5.78125 24.78125 L 7.21875 26.21875 L 16 17.4375 L 24.78125 26.21875 L 26.21875 24.78125 L 17.4375 16 L 26.21875 7.21875 L 24.78125 5.78125 L 16 14.5625 Z "/>
+                        </svg>
+                    </div>
+                </div>
             </header>
             <div class="email-box">
-                <span class="email">sean.zhou@connect.ust.hk</span>
+                <a href="mailto:sean.zhou@connect.ust.hk" class="email">sean.zhou@connect.ust.hk</a>
             </div>
         </div>
     </section>
@@ -31,7 +47,15 @@ export default {
                     method: 'Instagram',
                     value: 'seanzhousiyuan'
                 }
-            ]
+            ],
+            overlay: {
+                open: false
+            }
+        }
+    },
+    methods: {
+        toggleOverlay() {
+            this.overlay.open = !this.overlay.open;
         }
     }
 }

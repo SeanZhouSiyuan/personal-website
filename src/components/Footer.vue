8af3391f30dce22020373b92e3fe61190c391b7c
SeanZhouSiyuan <zhousiyuan12321@gmail.com>
2019-02-26 16:23:27 +0800

    set up navigation links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,43 @@
 <template>
     <footer>
         <div class="wrapper">
-            <nav>
-                <div
-                    v-for="(entry, index) in nav"
-                    :key="index"
-                    class="single-list"
-                >
-                    <h4 class="title">{{ entry.title }}</h4>
-                    <ul>
-                        <li
-                            @click="handleNavigation(entry.title, item.name)"
-                            v-for="(item, index) in entry.items"
-                            :key="index"
-                        >{{ item.name }}</li>
-                    </ul>
-                </div>
-            </nav>
+            <div class="navigation">
+                <nav>
+                    <div class="single-list">
+                        <h4 class="title">About</h4>
+                        <ul>
+                            <li class="list-item">
+                                <span @click="showCategory(1)">Education</span>
+                            </li>
+                            <li class="list-item">
+                                <span @click="showCategory(2)">Skills</span>
+                            </li>
+                            <li class="list-item">
+                                <span @click="showCategory(3)">Experiences</span>
+                            </li>
+                            <li class="list-item">
+                                <span @click="showCategory(4)">Misc</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="single-list">
+                        <h4 class="title">Contact</h4>
+                        <ul>
+                            <li class="list-item"><a href="mailto:sean.zhou@connect.ust.hk">Email</a></li>
+                            <li class="list-item"><a href="https://github.com/SeanZhouSiyuan" target="_blank">GitHub</a></li>
+                            <li class="list-item"><a href="https://www.facebook.com/seanzhousiyuan" target="_blank">Facebook</a></li>
+                            <li class="list-item"><span @click="showCode">WeChat</span></li>
+                        </ul>
+                    </div>
+                    <div class="single-list">
+                        <h4 class="title">Articles</h4>
+                        <ul>
+                            <li class="list-item list-item--link"><router-link to="/posts">Latest posts</router-link></li>
+                            <li class="list-item list-item--link"><router-link to="/posts">Tags</router-link></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
             <div class="copyright">
                 <span class="text">© 2019 Sean Zhou</span>
             </div>
@@ -25,48 +46,15 @@
 </template>
 
 <script>
+import { eventBus } from './../eventBus.js';
 export default {
     name: 'HomeFooter',
-    data() {
-        return {
-            nav: [
-                {
-                    title: 'About',
-                    items: [
-                        {
-                            name: 'Education'
-                        }, {
-                            name: 'Skills'
-                        }, {
-                            name: 'Experiences'
-                        }, {
-                            name: 'Misc'
-                        }
-                    ]
-                }, {
-                    title: 'Contact',
-                    items: [
-                        {
-                            name: 'Email'
-                        }, {
-                            name: 'GitHub'
-                        }, {
-                            name: 'Facebook'
-                        }, {
-                            name: 'WeChat'
-                        }
-                    ]
-                }, {
-                    title: 'Articles',
-                    items: [
-                        {
-                            name: 'Latest posts'
-                        }, {
-                            name: 'Tags'
-                        }
-                    ]
-                }
-            ]
+    methods: {
+        showCategory(id) {
+            eventBus.$emit('showCategory', id);
+        },
+        showCode() {
+            eventBus.$emit('showCode');
         }
     }
 }
